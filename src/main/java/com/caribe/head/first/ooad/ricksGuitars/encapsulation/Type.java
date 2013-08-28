759d5115029c40f6dea72c7417cb3d7c27716b71
thinkdeeply <rockflyrain@gmail.com>
2013-08-28 20:24:54 +0800

    Initialize project.

---
+++ src/main/java/com/caribe/head/first/ooad/ricksGuitars/encapsulation/Type.java
@@ -0,0 +1,17 @@
+package com.caribe.head.first.ooad.ricksGuitars.encapsulation;
+
+public enum Type {
+
+	ACOUSTIC, ELECTRIC;
+
+	public String toString() {
+		switch (this) {
+		case ACOUSTIC:
+			return "acoustic";
+		case ELECTRIC:
+			return "electric";
+		default:
+			return "unspecified";
+		}
+	}
+}
